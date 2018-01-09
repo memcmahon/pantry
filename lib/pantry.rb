419a666c24f9ca22f2025a7575c505334d588967
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-01-09 09:16:29 -0700

    Adds methods to fulfill iteration 1
    Includes methods and tests for pantry: stock, stock_check, restock

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,23 @@
 class Pantry
+  attr_reader :stock
 
+  def initialize
+    @stock = {}
+  end
+
+  def stock
+    @stock
+  end
+
+  def stock_check(item)
+    if @stock[item].nil?
+      0
+    else
+      @stock[item]
+    end
+  end
+
+  def restock(item, amount)
+    @stock[item] = stock_check(item) + amount
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,29 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  def test_it_exists
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_starts_empty
+    pantry = Pantry.new
+
+    assert_equal({}, pantry.stock)
+  end
+
+  def test_it_can_count_specific_items
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("cheese")
+  end
+
+  def test_it_can_restock_specific_items_with_amounts
+    pantry = Pantry.new
+
+    assert_equal 10, pantry.restock("Cheese", 10)
+    assert_equal 30, pantry.restock("Cheese", 20)
+  end
+
 end
